8287f58e5c2ca5fb444ed6c45a104d15ea8c39a1
zubair7384 <zubairiqbal@uconnect.pk>
2019-01-02 11:37:58 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// let val;
+
+// val = document;
+// val = document.all;
+// val = document.all[2];
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
+
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
+// val = document.forms[0].action;
+
+// val = document.links;
+// val = document.links[0];
+// val = document.links[0].id;
+// val = document.links[0].className;
+// val = document.links[0].classList[0];
+
+// val = document.images;
+
+// val = document.scripts;
+// val = document.scripts[2].getAttribute('src');
+
+// let scripts = document.scripts;
+
+// let scriptsArr = Array.from(scripts);
+
+// scriptsArr.forEach(function(script) {
+//   console.log(script.getAttribute('src'));
+// });
+
+// console.log(val);
+document.querySelector(".card-title").style.background = "black";
+document.querySelector(".card-title").style.borderRadius = ".2rem";
+document.querySelector(".card-title").style.color = "#fff";
+document.querySelector(".card-title").style.padding = ".5rem 2rem .5rem 1rem";
+document.getElementById("task-title").style.background = "green";
+document.getElementById("task-title").style.color = "#fff";
+document.getElementById("task-title").style.padding = ".7rem 2rem .7rem 1rem";
+document.getElementById("task-title").style.borderRadius = ".2rem";
+document.querySelector(".collection-item").style.color = "#26a69a";
+// const lis = (document.getElementsByTagName("li").style.background = "black");
+// console.log(lis);
+// console.log(lis[0]);
+// items[1].textContent = "yellow";
